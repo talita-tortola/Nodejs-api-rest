6799358f46f3279c48e387596a7528ecf332ef57
Talita Tortola <62050109+talita-tortola@users.noreply.github.com>
2022-07-06 22:55:35 -0300

    Add files via upload

---
+++ agenda-petshop/controllers/atendimentos.js
@@ -0,0 +1,37 @@
+const atendimentos = require('../models/atendimentos')
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        Atendimento.busca(req.params, res)
+
+        //outro modo de fazer:
+        // const id = parseInt(req.params.id)
+        // Atendimentos.buscaPorId(id, res)
+        
+    })
+    
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+        
+        Atendimento.adiciona(atendimento, res)
+        
+    })
+    
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
+    })
+}
